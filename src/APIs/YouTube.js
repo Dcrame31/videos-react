83a433d454aa467c674420aa650a7dfcb7d2d8d7
Diane Crame <dcrame@icloud.com>
2021-01-11 18:44:36 -0800

    add axios and youtube api

---
+++ src/APIs/YouTube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAgwdWssyknW9u3pV4O7SEt916RhK7AG_o';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+});
