19f47644ac675d2eb9bc98cd050a386e167e58e3
Diane Crame <dcrame@icloud.com>
2021-01-11 17:36:04 -0800

    add event listeners

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onInputChange = (e) => {
+        this.setState({ term: e.target.value})
+    };
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.setState({term: ''})
+    };
+
     render() {
         return (
-            <div>
-                Search Bar
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <input onChange={this.onInputChange} value={this.state.term} type="text"></input>
+                        <label>Search Bar</label>
+                    </div>
+                </form>
             </div>
         )
     }
